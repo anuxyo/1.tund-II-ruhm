cebc11f55070edb385605670e90060157940abfb
anusada <anusada@greeny.cs.tlu.ee>
2016-09-06 15:16:52 +0300

    added my first file

---
+++ first.php
@@ -0,0 +1,17 @@
+
+<?php
+
+	$firstname = "Anu";
+	$lastname = "Sadam";
+	
+	//echo trükib välja muutuja veebilehel
+	
+	//echo $firstname;
+	//echo " ";
+	//echo $lastname;
+	
+	//punkt liidab stringe
+	
+	echo $firstname. " ".$lastname;
+
+?>
